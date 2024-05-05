3c84fc3617e3fd6cc6830bb7e5b96a9c102924e0
ilija Kekovic <ilijakekovic@gmail.com>
2024-05-05 15:59:35 -0300

    Updated app.js

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -10,6 +10,8 @@ const division = $('numDivision');
 const multiply = $('numMultiply')
 const subtract = $('numSubtraction');
 const add = $('numAddition');
+const equals = $('equals');
+const output = $('returnOutput');
 const num1 = $('num1');
 const num2= $('num2');
 const num3= $('num3');
@@ -19,31 +21,41 @@ const num6= $('num6');
 const num7= $('num7');
 const num8= $('num8');
 const num9= $('num9');
+const num0= $('zero');
 //#endregion
 let timeForNum1 = true;
+let equation = "";
+let result = 0;
 
 root.addEventListener('click', function() {
     timeForNum1 = false;
+    equation = "root";
     console.log(timeForNum1);
 });
 percentage.addEventListener('click', function() {
     timeForNum1 = false;
+    equation = "percentage";
     console.log(timeForNum1);
 });
 division.addEventListener('click', function() {
     timeForNum1 = false;
+    equation = "division";
     console.log(timeForNum1);
 });
 multiply.addEventListener('click', function() {
     timeForNum1 = false;
+    equation = "multiply";
     console.log(timeForNum1);
 });
 subtract.addEventListener('click', function() {
     timeForNum1 = false;
+    equation = "subtract";
     console.log(timeForNum1);
 });
 add.addEventListener('click', function() {
     timeForNum1 = false;
+    equation = "add";
+    console.log(equation);
     console.log(timeForNum1);
 });
 
@@ -124,6 +136,14 @@ num9.addEventListener('click', function() {
         number2 += "9";
     }
 });
+num0.addEventListener('click', function() {
+    if(timeForNum1){
+        number1 += "0";
+    }
+    else{
+        number2 += "0";
+    }
+});
 
 AC.addEventListener('click', function() {
     number1 = "";
@@ -134,4 +154,35 @@ AC.addEventListener('click', function() {
 let number1Int = 0;
 let number2Int = 0;
 
-equal
+equals.addEventListener('click', function() {
+    console.log(`equals click`);
+    number1Int = parseInt(number1);
+    number2Int = parseInt(number2);
+    if(equation == "percentage"){
+        result = (number1Int / 100);
+        console.log('percentage');
+    }
+    else if(equation == "division"){
+        result = (number1Int / number2Int);
+        console.log('division');
+    }
+    else if(equation == "multiply"){
+        result = (number1Int * number2Int);
+        console.log('multiply');
+    }
+    else if(equation == "subtract"){
+        result = (number1Int - number2Int);
+        console.log('subtract');
+    }
+    else if(equation == "add"){
+        result = (number1Int + number2Int);
+        console.log('add');
+    }
+    console.log(result);
+});
+
+root.addEventListener('click', function() {
+    result = Math.sqrt(number1Int);
+});
+
+output.innerHTML = result;
