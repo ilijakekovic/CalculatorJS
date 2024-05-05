69edaf100be3351ca3e86800deef1d7ed03226e8
ilija Kekovic <ilijakekovic@gmail.com>
2024-05-05 04:25:03 -0300

    Added a JS file

---
+++ JavaScript/app.js
@@ -0,0 +1,24 @@
+function $(id){
+    return document.getElementById(id);
+}
+
+//#region Doms
+const AC = $('numAC');
+const root = $('numRoot');
+const percentage = $('numPercentage');
+const division = $('numDivision');
+const multiply = $('numMultiply')
+const subtract = $('numSubtraction');
+const add = $('numAddition');
+const num1 = $('num');
+const num2= $('num');
+const num3= $('num');
+const num4= $('num');
+const num5= $('num');
+const num6= $('num');
+const num7= $('num');
+const num8= $('num');
+const num9= $('num');
+//#endregion
+
+let 
